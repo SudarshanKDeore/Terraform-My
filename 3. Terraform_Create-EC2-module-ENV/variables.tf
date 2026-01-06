a108cfa9088b177ddc4300ce0a2c0fec871367bc
Sudarshan Deore <122254130+SudarshanKDeore@users.noreply.github.com>
2026-01-06 18:15:43 +0530

    Update variables.tf

--- 3. Terraform_Create-EC2-module-ENV/variables.tf
+++ 3. Terraform_Create-EC2-module-ENV/variables.tf
@@ -1,19 +1,25 @@
 variable "aws_region" {
-  description = "AWS region"
-  type        = string
+  type = string
 }
 
 variable "ami_id" {
-  description = "AMI ID"
-  type        = string
+  type = string
 }
 
 variable "instance_type" {
-  description = "EC2 instance type"
-  type        = string
+  type = string
 }
 
 variable "key_name" {
-  description = "EC2 key pair name"
+  type = string
+}
+
+variable "environment" {
+  description = "Environment name (test, staging, prod)"
+  type        = string
+}
+
+variable "s3_bucket" {
+  description = "S3 bucket name for EC2 access"
   type        = string
 }
