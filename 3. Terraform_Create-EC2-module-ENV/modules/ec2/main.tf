f73b9bd45822c799c5adea1136253fb26067dd3f
Sudarshan Deore <122254130+SudarshanKDeore@users.noreply.github.com>
2026-01-06 18:14:37 +0530

    Update main.tf

--- 3. Terraform_Create-EC2-module-ENV/modules/ec2/main.tf
+++ 3. Terraform_Create-EC2-module-ENV/modules/ec2/main.tf
@@ -1,20 +1,9 @@
 #############################
-# Generate SSH Key Pair
+# SSH Key
 #############################
 resource "tls_private_key" "ec2_key" {
   algorithm = "RSA"
   rsa_bits  = 4096
-}
-
-resource "local_file" "private_key" {
-  content         = tls_private_key.ec2_key.private_key_pem
-  filename        = "${path.root}/${var.key_name}.pem"
-  file_permission = "0400"
-}
-
-resource "local_file" "public_key" {
-  content  = tls_private_key.ec2_key.public_key_openssh
-  filename = "${path.root}/${var.key_name}.pub"
 }
 
 resource "aws_key_pair" "this" {
@@ -33,9 +22,8 @@ data "aws_vpc" "default" {
 # Security Group
 #############################
 resource "aws_security_group" "this" {
-  name        = "ec2_security_group"
-  description = "Allow SSH"
-  vpc_id      = data.aws_vpc.default.id
+  name   = "ec2_security_group"
+  vpc_id = data.aws_vpc.default.id
 
   ingress {
     from_port   = 22
@@ -50,21 +38,68 @@ resource "aws_security_group" "this" {
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
+
+  tags = {
+    Environment = var.environment
+  }
+}
+
+#############################
+# IAM Role + S3 Access
+#############################
+resource "aws_iam_role" "ec2_role" {
+  name = "ec2-s3-role-${var.environment}"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect = "Allow"
+      Principal = {
+        Service = "ec2.amazonaws.com"
+      }
+      Action = "sts:AssumeRole"
+    }]
+  })
+}
+
+resource "aws_iam_policy" "s3_policy" {
+  name = "ec2-s3-policy-${var.environment}"
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect = "Allow"
+      Action = ["s3:*"]
+      Resource = [
+        "arn:aws:s3:::${var.s3_bucket}",
+        "arn:aws:s3:::${var.s3_bucket}/*"
+      ]
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "attach" {
+  role       = aws_iam_role.ec2_role.name
+  policy_arn = aws_iam_policy.s3_policy.arn
+}
+
+resource "aws_iam_instance_profile" "this" {
+  name = "ec2-profile-${var.environment}"
+  role = aws_iam_role.ec2_role.name
 }
 
 #############################
 # EC2 Instance
 #############################
 resource "aws_instance" "this" {
-  ami           = var.ami_id
-  instance_type = var.instance_type
-  key_name      = aws_key_pair.this.key_name
-
-  vpc_security_group_ids = [
-    aws_security_group.this.id
-  ]
+  ami                    = var.ami_id
+  instance_type          = var.instance_type
+  key_name               = aws_key_pair.this.key_name
+  iam_instance_profile   = aws_iam_instance_profile.this.name
+  vpc_security_group_ids = [aws_security_group.this.id]
 
   tags = {
-    Name = "Terraform-EC2"
+    Name        = "Terraform-EC2"
+    Environment = var.environment
   }
 }
