32b36af9cd175a106a4bfd8cea698eb60f25380b
Sudarshan Deore <122254130+SudarshanKDeore@users.noreply.github.com>
2026-01-06 18:17:31 +0530

    Create backend-staging.tf

---
+++ 3. Terraform_Create-EC2-module-ENV/backend-staging.tf
@@ -0,0 +1,9 @@
+terraform {
+  backend "s3" {
+    bucket         = "terraform-state-staging-123"
+    key            = "ec2/staging/terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "terraform-locks"
+    encrypt        = true
+  }
+}
